ca54f522291544a4a5f6935172e18d743f369925
Ilya Khorev <ilya.khorev@waveaccess.ru>
2020-04-18 20:46:00 +0300

    Azure Resources deploy scripts

--- Scripts/AzureResources.ps1
+++ Scripts/AzureResources.ps1
@@ -1,12 +1,58 @@
-blobStorageAccount="<blob_storage_account>"
+# To run with PowerShell execute the following command: 
+# .\AzureResources.ps1
+# If Azure module is not installed, run the command below as administrator:
+# Install-Module -Name Az -AllowClobber
 
-az storage account create --name $blobStorageAccount --location southeastasia --resource-group myResourceGroup --sku Standard_LRS --kind blobstorage --access-tier hot
+. "$PSScriptRoot\Common.ps1"
 
-blobStorageAccountKey=$(az storage account keys list -g myResourceGroup -n $blobStorageAccount --query "[0].value" --output tsv)
+CheckLoggedIn
 
-az storage container create -n images --account-name $blobStorageAccount --account-key $blobStorageAccountKey --public-access off
+# Specify your resource group, subscription, and location here
+$resourceGroup = "TestResourceGroup"
+$location = "westeurope"
+$subscriptionName = ""
 
-az storage container create -n thumbnails --account-name $blobStorageAccount --account-key $blobStorageAccountKey --public-access container
+# Selecting an appropriate subscription
+Set-AzContext -Subscription $subscriptionName
 
-echo "Make a note of your Blob storage account key..."
-echo $blobStorageAccountKey
+# Creating blob storage
+$accountName = "imagesstorageaccount"
+New-AzStorageAccount -ResourceGroupName $resourceGroup `
+  -Name $accountName `
+  -Location $location `
+  -SkuName Standard_LRS `
+  -Kind StorageV2
+
+# Creating Cosmos DB
+$accountName = "imagemetadatadb"
+$apiKind = "GlobalDocumentDB"
+New-AzCosmosDBAccount -ResourceGroupName $resourceGroup `
+      -Location $location -Name $accountName `
+      -ApiKind $apiKind -EnableAutomaticFailover:$true `
+
+# Creating Service Bus
+# Query to see if the namespace currently exists
+$namespace = "myservicebus"
+$CurrentNamespace = Get-AzServiceBusNamespace -ResourceGroup $resourceGroup -NamespaceName $namespace
+# Check if the namespace already exists or needs to be created
+if ($CurrentNamespace)
+{
+    Write-Host "The namespace $Namespace already exists in the $location region:"
+ 	# Report what was found
+ 	Get-AzServiceBusNamespace -ResourceGroup $resourceGroup -NamespaceName $namespace
+}
+else
+{
+    Write-Host "The $Namespace namespace does not exist."
+    Write-Host "Creating the $Namespace namespace in the $location region..."
+    New-AzServiceBusNamespace -ResourceGroup $resourceGroup -NamespaceName $namespace -Location $location
+    $CurrentNamespace = Get-AzServiceBusNamespace -ResourceGroup $resourceGroup -NamespaceName $namespace
+    Write-Host "The $Namespace namespace in Resource Group $resourceGroup in the $location region has been successfully created."
+}
+
+#Creating App Insights
+$appInsightsName = "imagesappinsights"
+New-AzApplicationInsights -ResourceGroupName $resourceGroup `
+  -Name $appInsightsName `
+  -location $location `
+  -Kind other 
