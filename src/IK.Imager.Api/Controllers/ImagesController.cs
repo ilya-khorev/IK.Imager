ffe14d1e4283df5800a2631b8e0a5bc6c1a295fc
Ilya Khorev <ilyakhorev89@gmail.com>
2021-11-30 18:13:41 +0300

    Leave only images controller

--- src/IK.Imager.Api/Controllers/ImagesController.cs
+++ src/IK.Imager.Api/Controllers/ImagesController.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using IK.Imager.Api.Commands;
 using IK.Imager.Api.Contract;
+using IK.Imager.Api.IntegrationEvents.Events;
 using IK.Imager.Api.Services;
 using MassTransit;
 using MediatR;
@@ -18,9 +19,9 @@ namespace IK.Imager.Api.Controllers
     [Produces("application/json")]
     [ApiController]
     [Route("[controller]")]
-    public class UploadController : ControllerBase
+    public class ImagesController : ControllerBase
     {
-        private readonly ILogger<UploadController> _logger;
+        private readonly ILogger<ImagesController> _logger;
         private readonly IPublishEndpoint _publishEndpoint;
         private readonly ImageDownloadClient _imageDownloadClient;
 
@@ -29,9 +30,10 @@ namespace IK.Imager.Api.Controllers
         private const string CouldNotDownloadImage = "Couldn't download an image by url {0}.";
         private const string DownloadingByUrl = "Downloading an image by url {0}.";
         private const string DownloadedByUrl = "Downloaded an image by url {0}.";
+        private const string ImageNotFound = "Requested image with id {0} was not found";
 
         /// <inheritdoc />
-        public UploadController(ILogger<UploadController> logger, IPublishEndpoint publishEndpoint, ImageDownloadClient imageDownloadClient, IMediator mediator)
+        public ImagesController(ILogger<ImagesController> logger, IPublishEndpoint publishEndpoint, ImageDownloadClient imageDownloadClient, IMediator mediator)
         {
             _logger = logger;
             _publishEndpoint = publishEndpoint;
@@ -50,10 +52,10 @@ namespace IK.Imager.Api.Controllers
         /// <response code="400">If the image size is greater or smaller then the system threshold values.
         /// Or if the image type is different from what the system supports.</response> 
         [HttpPost]
+        [Route("Upload")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
         [Consumes("multipart/form-data")]
-        //todo probably worth uploading using stream https://docs.microsoft.com/en-us/aspnet/core/mvc/models/file-uploads?view=aspnetcore-3.1#upload-large-files-with-streaming
         public async Task<ActionResult<ImageInfo>> Post([FromForm]UploadImageFileRequest imageFileRequest)
         {
             return await UploadImage(imageFileRequest.File.OpenReadStream(), imageFileRequest.ImageGroup);
@@ -73,7 +75,7 @@ namespace IK.Imager.Api.Controllers
         /// Or if the image size is greater or smaller then the system threshold values.
         /// Or if the image type is different from what the system supports.</response> 
         [HttpPost]
-        [Route("ByUrl")]
+        [Route("UploadByUrl")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<ImageInfo>> PostByUrl(UploadImageRequest uploadImageRequest)
@@ -109,6 +111,56 @@ namespace IK.Imager.Api.Controllers
             });
             
             return Ok(uploadImageResult);
+        }
+        
+        /// <summary>
+        /// Search for set of images by image ids
+        /// </summary>
+        /// <param name="searchImagesByIdRequest">Search image request model</param>
+        /// <returns>A model with full info about just found images. Each image is represented with the nested object.
+        /// These objects are returned in the same order as they were requested.
+        /// If some image is not found, this image is returned as null object.</returns>
+        /// <response code="200">Returns information about images.</response>
+        /// <response code="400">If the image id is not specified.
+        /// Or if it is requested for more than 200 images.</response> 
+        [HttpPost]
+        [Route("Search")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<ImagesSearchResult>> Post(SearchImagesByIdRequest searchImagesByIdRequest)
+        {
+            var uploadImageResult = await _mediator.Send(new RequestImagesCommand(searchImagesByIdRequest.ImageIds, searchImagesByIdRequest.ImageGroup));
+            return uploadImageResult;
+        }
+        
+        /// <summary>
+        /// Register the image removal by the given image id. 
+        /// The system will remove the original image and thumbnail files after a short delay.
+        /// However, the image itself will stop to return in search results immediately after this call.  
+        /// </summary>
+        /// <returns></returns>
+        /// <response code="204">The image has been removed.</response>
+        /// <response code="400">If the image id is not specified.</response> 
+        /// <response code="404">The requested image was not found.</response> 
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(DeleteImageRequest deleteImageRequest)
+        {
+            var imageDeleteResult = await _mediator.Send(new RemoveImageCommand(deleteImageRequest.ImageId, deleteImageRequest.ImageGroup));
+            
+            if (imageDeleteResult == null)
+                return NotFound(string.Format(ImageNotFound, deleteImageRequest.ImageId));
+           
+            await _publishEndpoint.Publish(new ImageDeletedIntegrationEvent
+            {
+                ImageId = imageDeleteResult.ImageId,
+                ImageName = imageDeleteResult.ImageName,
+                ThumbnailNames = imageDeleteResult.ThumbnailNames
+            });
+
+            return NoContent();
         }
     }
 }
